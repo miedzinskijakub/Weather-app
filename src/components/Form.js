3da28d1ad505c897c7b7c55053807bd2114edc8b
miedzinskijakub <47180627+miedzinskijakub@users.noreply.github.com>
2020-12-26 14:11:16 +0100

    q

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ import './App.css';
 const Form = props => {
     return (
             <input type="text"
-            autofocus="true"
+            autoFocus
             placeholder="Search for a city..."
             value={props.value}
             onChange={props.change}
--- src/components/Result.js
+++ src/components/Result.js
@@ -13,15 +13,13 @@ const icons = `http://openweathermap.org/img/wn/${icon}@2x.png`
 
         content = (
 
-
-
         <div>
-            <h3 class="city">{city}, {country}</h3>
-            <h4 class="date">{date}</h4>
-            <h1 class="temp">{temp} &#176;C</h1>
-            <h3 class="weather">{weather} </h3>
+            <h3 className="city">{city}, {country}</h3>
+            <h4 className="date">{date}</h4>
+            <h1 className="temp">{temp} &#176;C</h1>
+            <h3 className="weather">{weather} </h3>
             <img alt="Icon" src={icons}></img>
-            <h4 class="tempMin">{tempMin}&#176; / {tempMax}&#176;</h4>
+            <h4 className="tempMin">{tempMin}&#176; / {tempMax}&#176;</h4>
   
         </div>
         )
@@ -29,8 +27,7 @@ const icons = `http://openweathermap.org/img/wn/${icon}@2x.png`
 
     return (
         <div className="result">
-            {err ? `` :
-            content}
+            {err ? `` : content}
         </div>
 
 
