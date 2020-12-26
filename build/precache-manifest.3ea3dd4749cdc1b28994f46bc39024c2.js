dafbc7571b58511915ff1cba52d609118f004dea
miedzinskijakub <47180627+miedzinskijakub@users.noreply.github.com>
2020-12-26 14:32:34 +0100

    q

---
+++ build/precache-manifest.3ea3dd4749cdc1b28994f46bc39024c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6faac61b466748d11953abb971970727",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "3bd4e920106669cb7869",
+    "url": "/Weather-app/static/css/main.2aa626df.chunk.css"
+  },
+  {
+    "revision": "7d7107bda963a4c40fc2",
+    "url": "/Weather-app/static/js/2.21581a3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.21581a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd4e920106669cb7869",
+    "url": "/Weather-app/static/js/main.4871a1cd.chunk.js"
+  },
+  {
+    "revision": "dc7fe64dca44833a815a",
+    "url": "/Weather-app/static/js/runtime-main.087a8e29.js"
+  }
+]);
